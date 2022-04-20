15046412a02c5e098e784ce93a852eaa31aa2254
yuchen <yuchen@holoi.com>
2022-04-20 13:33:46 +0800

    Initial Commit

---
+++ permissions-oc/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  permissions-oc
+//
+//  Created by Yuchen Zhang on 2022/4/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
